4585a52126ce9083df936594aebfbe57613990b6
Jimut Dhali <jemdhali67@gmail.com>
2017-05-19 16:44:10 +0530

    Fixed the issue for downloading urls with uppercase letters

--- src/ExportHandler/Services/EmbedderService.php
+++ src/ExportHandler/Services/EmbedderService.php
@@ -8,7 +8,7 @@ use GuzzleHttp\Exception\ConnectException;
 
 class EmbedderService
 {
-    protected $imageLinkRegex = '/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/';
+    protected $imageLinkRegex = '/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i';
 
     public function serve($stream)
     {
