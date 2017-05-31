20fe037a8f11053015aabe1948f54ded7bc996a5
Jimut Dhali <jemdhali67@gmail.com>
2017-05-31 17:05:31 +0530

    Added support code for getallheader method

--- src/ExportHandler/sanitizer.php
+++ src/ExportHandler/sanitizer.php
@@ -1,4 +1,21 @@
 <?php
+
+if (!function_exists('getallheaders'))
+{
+    function getallheaders()
+    {
+        $headers = array ();
+        foreach ($_SERVER as $name => $value)
+        {
+            if (substr($name, 0, 5) == 'HTTP_')
+            {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
 /**
  *
  * Takes $_POST sanitizes and returns $sanitized_post
